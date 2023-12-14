43c69e34acd79fe5d65f08b6663b9db655e2a520
Mohit Tomer <mstomer31@gmail.com>
2023-12-14 19:32:04 +0530

    Initial Commit

---
+++ SwiftUi-Weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUi-Weather
+//
+//  Created by Mohit Tomer on 14/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
